1a61d1b1ae96dcb702c1d094d2bad4cdbcd05a2b
PaulaVelezz <velezpaula.a@gmail.com>
2024-11-22 09:28:40 -0300

    4.3

--- src/components/Certifications/Certificate.jsx
+++ src/components/Certifications/Certificate.jsx
@@ -3,24 +3,19 @@ import { motion } from 'framer-motion';
 
 const Certificate = ({certificate}) => {
   const {firstTitle, secondTitle, image} = certificate;
-  const [ isActive, setIsActive ] = useState(false);
-
-  const handleMouseEnter = () => setIsActive(true);
-  const handleMouseLeave = () => setIsActive(false);
-
 
   return (
-    <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='w-full flex items-center justify-center gap-4 border-t-[1px] border-violet-700 text-stone-200 pt-[.5vw] pb-[.5vw]'>
-      <motion.div animate={isActive ? "open" : "closed"} className='w-44 h-44'>
+    <div className='lg:w-full lg:flex lg:items-center lg:justify-between gap-4 border-t-[1px] border-violet-700 text-stone-200 p-4'>
+      <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} className='w-full lg:w-1/4'>
         <a href={image} target="_blank">
-          <img src={image} alt="certificate" className='w-44 h-44 object-contain' />
+          <img src={image} alt="certificate" width={150} height={150} className='object-contain rounded' />
         </a>
       </motion.div>
 
-      <div className='w-full gap-3 flex flex-col items-end justify-center'>
-        <p className='text-2xl md:text-3xl lg:text-4xl'>{firstTitle}</p>
-        <span className='text-lime-400'>{secondTitle}</span>
-      </div>
+      <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1 }} className='w-full max-w-xl lg:w-3/4'>
+        <p className='mb-2 text-2xl md:text-3xl lg:text-4xl'>{firstTitle}</p>
+        <span className='mb-2 text-lime-400'>{secondTitle}</span>
+      </motion.div>
     </div>
   )
 }
